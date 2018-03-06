d931b8c081f7a17acf79ab2f333546c633bd159d
marcv3 <marc_v3@yahoo.com>
2018-03-05 18:32:01 -0800

    added chapters 3 and 4

--- clean.sh
+++ clean.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-cd ch1
+cd ch1_a-tutorial-introduction
 if [ -d "work" ]; 
 then make clean_work
 else echo ""
 fi
 cd ..
 
-cd ch2
+cd ch2_types-operators-expressions
+if [ -d "work" ]; 
+then make clean_work
+else echo ""
+fi
+cd ..
+
+cd ch3_control-flow
+if [ -d "work" ]; 
+then make clean_work
+else echo ""
+fi
+cd ..
+
+cd ch4_functions-and-prog-structure
 if [ -d "work" ]; 
 then make clean_work
 else echo ""
